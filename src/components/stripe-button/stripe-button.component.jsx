78168cfb19a44abb15b2c8265225e198179d382a
Manolo Gonzalez <manologonz203@gmail.com>
2020-05-03 11:38:09 -0600

    ADD: favicon and stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_sfmlfZeces2UoOSKxQMNIset00E6aGB05z"
+    const onToken = token => {
+        alert("Payment Successfull")
+        console.log(token);
+    }
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="CROWN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}/>
+    );
+};
+
+export default StripeCheckoutButton;
